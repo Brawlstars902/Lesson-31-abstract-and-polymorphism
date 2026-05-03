71aa4d8ebeb8647a3475fc9cfaa94d9bfd4fa0b1
Brawlstars902 <aparna.rautela@gmail.com>
2026-05-03 05:43:54 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,26 @@
+
+
+class BMW ():
+    def Intro(self):
+        print('\n\nI am a BMW: ')
+    def Fuel_Type(self):
+        print('My fuel type is Premium Gasoline')
+    def Max_Speed(self):
+        print('My maximum speed is 305 km/h')
+
+class Ferrari():
+    def Intro(self):
+        print('\nI am a Ferrari: ')
+    def Fuel_Type(self):
+        print('My fuel type is Premium Petrol')
+    def Max_Speed(self):
+        print('My maximum speed is 398.4 km/h\n\n')
+
+Car1=BMW()
+Car1.Intro()
+Car1.Fuel_Type()
+Car1.Max_Speed()
+Car2=Ferrari()
+Car2.Intro()
+Car2.Fuel_Type()
+Car2.Max_Speed()
